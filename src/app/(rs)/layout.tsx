d1c7813cc9293336c077597b5a81a081ac233062
Rohit Singh <55020650+RhtWeb@users.noreply.github.com>
2025-07-19 23:16:30 +0530

    folder Structure

---
+++ src/app/(rs)/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header"
+
+export default async function RSLayout({
+  children,
+}: {
+  children : React.ReactNode
+}){
+  return (
+    <div>
+      <Header />
+      <div>
+        {children}
+      </div>
+    </div>
+  )
+}
